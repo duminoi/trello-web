f5e8e8abe65f74bcd83c8bc3540a6e76af63156f
Duminoi <dminso1koaiso2@gmail.com>
2024-12-07 15:29:55 +0700

    Update height

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,6 +1,9 @@
 // import React from 'react'
 import Box from '@mui/material/Box'
 
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTERf_HEIGHT = '50px'
+
 export default function BoardContent() {
   return (
     <Box
@@ -8,13 +11,26 @@ export default function BoardContent() {
         bgcolor: (theme) =>
           theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
         width: '100%',
-        height: (theme) =>
-          `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        display: 'flex',
-        alignItems: 'center'
+        height: (theme) => theme.trello.boardContentHeight,
+        display: 'flex' 
+        // alignItems: 'center'
       }}
     >
-      Board content
+      {/* Box Column */}
+      <Box
+        sx={{
+          minWidth: '300px',
+          maxWidth: '300px',
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
+          ml: '2px',
+          borderRadius: '6px'
+        }}
+      >
+        <Box sx={{}}>Header</Box>
+        <Box sx={{}}>List card</Box>
+        <Box sx={{}}>Footer</Box>
+      </Box>
     </Box>
   )
 }
